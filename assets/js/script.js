2bb9cf4e65bc178281280cae9afd2f0ae3aa1b09
Scott Rohrig <scott.rohrig@gmail.com>
2021-12-06 17:49:32 -0800

    design project pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
-var message = config.SECRET_MSG
-var key = config.MY_API_KEY
+/*
+API calls
+    - use one-call-api <https://openweathermap.org/api/one-call-api>
+        - current > current.uvi|.temp|.humidity|.wind_speed|
+        - requires lon, lat
+        - https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    - get lon, lat from city search
+    - api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={API_KEY}
+
+*/ 
+
+// global variables
+var apiKey = config.API_KEY
+
+// refs to static html elements
+// search, location buttons, city stats, forecast
+
+
+// Get weather data
+function getWeatherData(location) {
+    // make api url
+    var apiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + apiKey
+    // fetch request
+    // response validation
+    // parse response
+ 
+}
+
+// get location coords
+
+// show info panel 
+// show forecast 
