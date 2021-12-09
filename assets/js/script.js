2ecef42d2a8829dd4ed35e70dbe2b2ac56eff69b
Scott Rohrig <scott.rohrig@gmail.com>
2021-12-08 23:20:26 -0800

    format script, reformat fetch calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,53 +15,46 @@ API calls
 */
 
 $(document).ready(function () {
-
-
     // global variables
     const apiKey = config.API_KEY;
     var searchHistory = [];
 
+    // storage handling
     function saveHistory() {
-        localStorage.setItem('location-history', JSON.stringify(searchHistory));
+        localStorage.setItem("location-history", JSON.stringify(searchHistory));
     }
 
     function loadHistory() {
-        var h = JSON.parse(localStorage.getItem('location-history'));
+        var h = JSON.parse(localStorage.getItem("location-history"));
         if (!h) {
             return false;
-
-        } 
+        }
         searchHistory = h;
-        $("#history").html('');
+        $("#history").html("");
 
         for (var location of searchHistory) {
             makeHistory(location);
         }
     }
-    
+
     function makeHistory(location) {
-        var li = $('<li>').addClass('btn btn-secondary mt-2').text(location)
+        var li = $("<li>").addClass("btn btn-secondary mt-2").text(location);
         $("#history").append(li);
     }
 
     loadHistory();
 
-    // refs to static html elements
-    // search, location buttons, city stats, forecast
-
     // Get weather data
     function getWeatherData(location) {
         location = location.trim();
-        if (!location) {
-            return false
-        } 
-        var state = '';
-        if (location.split(',')) {
-            state = ',' + location.split(',')[1]
+
+        var state = "";
+        if (location.split(",")) {
+            state = "," + location.split(",")[1];
         }
 
         // make api url
-        var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}${state}&appid=${apiKey}`
+        var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}${state}&appid=${apiKey}`;
 
         // fetch request
         fetch(apiUrl).then(function (response) {
@@ -69,93 +62,134 @@ $(document).ready(function () {
             if (response.ok) {
                 // parse response
                 response.json().then(function (data) {
-                    showCurrentWeather(data, location, state);
-                })
+                    showCurrentWeather(data, location);
+                });
             } else {
-                console.log('response error: ' + response.status);
+                console.log("response error: " + response.status);
             }
-        })
-
-        
-
+        });
     }
 
-    $('#search-form').submit(function (e) {
+    // on search submit
+    $("#search-form").submit(function (e) {
         e.preventDefault();
-        var location = $('#search').val();
-        console.log('location: ' + location);
+        var location = $("#search").val();
+        if (!location) {
+            return false;
+        }
+        console.log("location: " + location);
         getWeatherData(location);
 
-        if (!searchHistory.includes(location)){
+        if (!searchHistory.includes(location)) {
             searchHistory.push(location);
         }
-        
+
         if (searchHistory.length > 5) {
             searchHistory.shift();
         }
         saveHistory();
         loadHistory();
+    });
+    
+    function convertKtoF(kelvin) {
+        var farenheit = ((kelvin - 273.15) * 1.8 + 32).toFixed(1);
+        return farenheit;
+    }
+    
+    // show current weather info panel
+    function showCurrentWeather(weather, location) {
 
-    })
+        var titleText = `Showing results for ${weather.name}, ${weather.sys.country}`;
+        
+
+        updateMapFrameSrc(`${location}`);
+        
+        var today = new Date();
+        var fmtToday = moment(today*1000).format('MM/DD/YYYY');
+        // todo: format with moment
+        
+        $("#current-weather h2").text(titleText);
+        // $("#current-weather h2").append("<i>").text('icon ' + today)
+        var dateSpan = $("<span>").text(fmtToday);
+        $("current-weather .card-header").append(dateSpan);
+        
+        var temp = convertKtoF(weather.main.temp);
+        var wind = weather.wind.speed;
+        var humidity = weather.main.humidity;
+
+        $("#current-temp span").text(temp);
+        $("#current-wind span").text(wind);
+        $("#current-humidity span").text(humidity);
+        getLocationCoords(weather);
+    }
     
     // get location coords
     function getLocationCoords(location) {
         // location is a city object
-        var lat = location.coord.lat
-        var lon = location.coord.lon
-        var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        var lat = location.coord.lat;
+        var lon = location.coord.lon;
+        var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}`;
         fetch(apiUrl).then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     showForecast(data);
-                })
+                });
             }
-        })
+        });
     }
     
-    function convertKtoF(kelvin) {
-        var farenheit = ((kelvin - 273.15) * 1.8 + 32).toFixed(1)
-        return farenheit
+    // show 5-day forecast
+    function showForecast(onecall) {
+
+        // set uvi 
+        // (delayed due to 2 fetches. consider populating current & forecast together)
+        var uvi = onecall.current.uvi;
+        setUVI(uvi);
+
+        // loop thru 5-day forecast
+        $('#forecast-wrapper').children().each(function (i) {
+
+            var date = onecall.daily[i].dt
+            var fmtDate = moment(date*1000).format('MM/DD/YYYY')
+            $(this).find('.date').text(fmtDate)
+
+            setForecastIcon(this, onecall, i)
+
+        })
+
+    }
+    
+    // updates 'uvi' span's value and bg-color
+    function setUVI(uvi) {
+        $("#current-uvi span").text(uvi);
+        $("#current-uvi span").removeClass();
+        if (uvi < 2) {
+            $("#current-uvi span").addClass("btn bg-success");
+        } else if (2 < uvi <= 5) {
+            $("#current-uvi span").addClass("btn bg-warning");
+        } else {
+            $("#current-uvi span").addClass("btn bg-danger");
+        }
     }
 
-    // show info panel 
-    function showCurrentWeather(weather, location, _state) {
-
-        var titleText = `Showing results for ${weather.name}, ${weather.sys.country} ${'today'}`
-
-        replaceLocationSrc(`${location}`)
-
-        $("#current-weather h2").text(titleText)
-
-        // console.log("showCurrentWeather executed", location, weather);
-        var temp = convertKtoF(weather.main.temp)
-        // var temp = weather.main.temp // convertKtoF(weather.main.temp)
-        var wind = weather.wind.speed
-        var humidity = weather.main.humidity
-        var uvi = '0.5'
-
-        $('#current-temp span').text(temp);
-        $('#current-wind span').text(wind);
-        $('#current-humidity span').text(humidity);
-        getLocationCoords(weather);
-    }
-    // show forecast 
-    function showForecast(data) {
-        // console.log('showForecast executed', data)
+    function setForecastIcon(inst, onecall, day) {
+        var icon = onecall.daily[day].weather[0].icon;
+        var t = $(inst).children();
+        console.log(t)
     }
 
-    function replaceLocationSrc(location) {
-        var src = $("#map iframe")[0]
-        location = location.split(' ').join('');
-        var regEx = /q=[\D\s]*(?=&)/g
-        var srcText = $(src).attr('src').replace(regEx, 'q='+location)
-        console.log('replaceLocationSrc', srcText)
-        $(src).attr('src', srcText);
+    function updateMapFrameSrc(location) {
+        var src = $("#map iframe")[0];
+        location = location.split(" ").join("");
+        var regEx = /q=[\D\s]*(?=&)/g;
+        var srcText = $(src)
+            .attr("src")
+            .replace(regEx, "q=" + location);
+        $(src).attr("src", srcText);
     }
 
-    $("#search-form").on('click','li', function(e) {
-        var result = $(this).text()
-        getWeatherData(result)
-    })
-
-})
+    $("#search-form").on("click", "li", function (e) {
+        var result = $(this).text();
+        getWeatherData(result);
+    });
+});
